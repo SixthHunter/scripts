49e5bcd61a812248317b30581a2b210ba51cafae
mountaineerbr <92537363+mountaineerbr@users.noreply.github.com>
2022-03-18 17:03:51 -0300

    Compatibilidade com links de domínio do UOL..

--- anta.sh
+++ anta.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # anta.sh -- puxa artigos da homepage de <oantagonista.com>
-# v0.19.2  feb/2022  by mountaineerbr
+# v0.19.3  mar/2022  by mountaineerbr
 
 #padrões
 
@@ -143,8 +143,8 @@ GARANTIA E REQUISITOS
 
 
 NOTAS E BUGS
-	Encanar a saída para um paginador como o \`Less' manualmente ou
-	ativando a opção -l do script em conjunto com a opção \`-r', buff-
+	Ao encanar a saída para um paginador como o \`Less' manualmente
+	ou ativar a opção -l do script em conjunto com a opção \`-r', buff-
 	eres do sistema não irá permitir que o script rode continuamente.
 	Para deixar o script rodando por tempo indefinido com a opção \`-r',
 	redirecione a saída para um arquivo.
@@ -190,7 +190,7 @@ EXEMPLOS DE USO
 
 		$ anta.sh -fr | less
 
-		$ anta.sh -fr >anta.txt ;tail anta.txt
+		$ anta.sh -fr >anta.txt
 
 
 	( 4 ) Textos completos dos artigos das primeiras 4 páginas
@@ -248,10 +248,10 @@ OPÇÕES
 	-v 	  Mostra a versão do script."
 
 #Orign servers
-SERVERS=(www.oantagonista.com)
-ALTSERVERS=(cache.oantagonista.com cms.oantagonista.com m.oantagonista.com wp.oantagonista.com editores.oantagonista.com)
+SERVERS=(https://oantagonista.uol.com.br/ www.oantagonista.com)
+ALTSERVERS=(cache.oantagonista.com m.oantagonista.com wp.oantagonista.com editores.oantagonista.com)
 #'52.204.39.109', '3.82.68.200', '18.204.255.62', '52.72.53.233', '34.198.178.99'
-#'54.210.110.10', '52.86.216.106', '34.207.47.120', '18.214.96.27'
+#'54.210.110.10', '52.86.216.106', '34.207.47.120', '18.214.96.27', cms.oantagonista.com
 
 AGENTS=('User-Agent: Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0)' 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1' 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41' 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36' 'User-Agent: Mozilla/5.0 (Linux; Android 6.0.1; SGP771 Build/32.2.A.0.253; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.9[<0;45;17M8 Safari/537.36' 'User-Agent: Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; RM-1127_16056) AppleWebKit/537.36(KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10536' 'User-Agent: Mozilla/5.0 (Linux; Android 6.0.1; SGP771 Build/32.2.A.0.253; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.98 Safari/537.36' 'User-Agent: Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36' 'user-agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Mobile Safari/537.36' 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586' 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0' 'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko' 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0' 'user-agent: Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Mobile Safari/537.36')
 
@@ -645,6 +645,7 @@ linksf() {
 			PAGINAS=0
 			COMP="${i,,}"
 			COMP="${COMP//https:\/\/www.oantagonista.com}"
+			COMP="${COMP//https:\/\/oantagonista.uol.com.br}"
 			fulltf ;((ret+=$?))
 		done
 	else
@@ -661,6 +662,8 @@ linksf() {
 
 			#pega links para artigos integrais
 			LINKS=$(getlinksf <<<"$PAGE")
+			LINKS="${LINKS//https:\/\/oantagonista.uol.com.br}"
+			LINKS="${LINKS//https:\/\/www.oantagonista.com}"
 
 			#crawl each link
 			while read COMP
@@ -675,7 +678,7 @@ linksf() {
 				#dont flood the server
 				sleep "$FLOOD"
 
-			done <<<"$(tac <<<"${LINKS//https:\/\/www.oantagonista.com/}" )"
+			done < <(tac <<<"$LINKS")
 			
 			sleep "$FLOOD"
 
